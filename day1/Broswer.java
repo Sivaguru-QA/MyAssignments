f30dfde1514e634f020d03535a75c1b323a45a15
Sivaguru-QA <sivaguru1208@gmail.com>
2024-05-27 23:49:19 +0530

    Add files via upload
    Day1

---
+++ day1/Broswer.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Broswer {
+
+	public static void main(String[] args) {
+		
+		int noofTab=7;
+		boolean pageLoaded=true;
+		char pageName='S';
+		String browserName="CHROME";
+		float browserVersion=123.89f;
+		long phNum=9876543210l;
+		
+		System.out.println("Number of tab opened :"+noofTab +"\n"+pageLoaded +"\n"+ pageName +"\n" 
+		+browserName +"\n"+ browserVersion +"\n"+browserVersion +"\n"+ phNum);
+		
+
+	}
+
+}
